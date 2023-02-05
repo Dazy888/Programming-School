c812a2d84ffb1647312152eb498777f54f20fbff
unknown <8205414@gmail.com>
2023-02-05 09:11:42 +0200

    Added Tailwind in the Footer component in the Home page

--- programming-school/src/components/home/footer/Footer.tsx
+++ programming-school/src/components/home/footer/Footer.tsx
@@ -2,10 +2,8 @@ import React from "react"
 
 const FooterComponent = () => {
     return(
-        <footer className={'flex-center'}>
-            <div>
-                <p>We use cookies to personalize services and improve the user experience of the site. If you do not agree to their use, please change your browser settings.</p>
-            </div>
+        <footer className={'py-8 text-center mt-12 text-lg text-white px-5'}>
+            <p>We use cookies to personalize services and improve the user experience of the site. If you do not agree to their use, please change your browser settings.</p>
         </footer>
     )
 }
